fa349c11016c1c9289d03c6e73f6d870e96d6060
charoenlap anatamsomat <charoenlap88@gmail.com>
2023-10-17 04:32:51 +0000

    update

--- src/pages/intrlligentDetail.js
+++ src/pages/intrlligentDetail.js
@@ -12,102 +12,183 @@ import { Breadcrumb,Menu } from 'antd';
 import { Layout,theme,  } from 'antd';
 import { LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import { Table, Tag } from 'antd';
-import MyModel2 from '@/components/myModel2';
-import { useRecoilState } from 'recoil';
-import { models2State, selectModel2State } from '@/store/data';
-import Link from 'next/link';
+
 const { Content,Sider  } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items2 = [
+  getItem(
+      <a href="/intrlligentDetail">Data Analytic</a>,
+      'intrlligentDetail',
+      <UserOutlined />,
+    ),
+    getItem(
+      <a href="/checkErrorCode">Check Error Code</a>,
+      'checkErrorCode',
+      <LaptopOutlined />,
+    ),
+    getItem(
+      <a href="/nvram">NVRAM Viewer</a>,
+      'nvram',
+      <LaptopOutlined />,
+    ),
+    getItem(
+      <a href="/serviceManual">Service Manual & Diagram</a>,
+      'serviceManual',
+      <LaptopOutlined />,
+    ),
+];
+// const columns = [
+//   {
+//     title: '',
+//     dataIndex: 0,
+//     key: '',
+//   },
+//   {
+//     title: '',
+//     dataIndex: 1,
+//     key: '',
+//   },
+//   {
+//     title: '',
+//     dataIndex: 2,
+//     key: '',
+//   },
+//   {
+//     title: '',
+//     dataIndex: 3,
+//     key: '',
+//   },
+//   {
+//     title: '',
+//     dataIndex: 4,
+//     key: '',
+//   },
+//   {
+//     title: '',
+//     dataIndex: 5,
+//     key: '',
+//   },
+//   {
+//     title: '',
+//     dataIndex: 6,
+//     key: '',
+//   },
+// ];
+
+// const data = [];
+
+// const props = {
+//   name: 'file',
+//   multiple: false,
+//   action: '/api/upload',
+//   method: 'post',
+// };
+const columns = [
+  {
+    title: 'Item',
+    dataIndex: 0,
+    key: 'col-0',
+  },
+  {
+    title: 'Current Value',
+    dataIndex: 1,
+    key: 'col-1',
+  },
+  {
+    title: 'Limit',
+    dataIndex: 2,
+    key: 'col-2',
+  },
+  {
+    title: 'Situation',
+    dataIndex: 3,
+    key: 'col-3'
+  },
+  {
+    title: 'End of life',
+    dataIndex: 4,
+    key: 'col-4',
+  },
+  {
+    title: '',
+    dataIndex: 5,
+    key: 'col-5',
+  },
+  {
+    title: '',
+    dataIndex: 6,
+    key: 'col-6',
+  },
+];
+const columnsResult = [
+  {
+    title: 'No',
+    dataIndex: 'no',
+    key: 'no',
+  },
+  {
+    title: 'Symptom / Detail',
+    dataIndex: 'symptom',
+    key: 'symptom',
+  },
+  {
+    title: 'Remedy',
+    dataIndex: 'remedy',
+    key: 'remedy',
+  },
+  {
+    title: 'Part Code',
+    dataIndex: 'part',
+    key: 'part',
+  },
+];
+const dataResult = [];
 const { Dragger } = Upload;
 const { Meta } = Card;
 
-export default function IntrlligentDetail() {
-  const [selectModel2, setSelectModel2] = useRecoilState(selectModel2State);
+const items = [
+  {
+    key: '1',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="#">
+        Model item
+      </a>
+    ),
+  },
+];
+const props = {
+  name: 'file',
+  multiple: false,
+  action: '/api/upload',
+  method: 'post',
+};
+const propsCalculate = {
+  name: 'file',
+  multiple: false,
+  action: '/api/analytic/readfile',
+  method: 'post',
+};
+export default function Index() {
   const [itemsModel, setItems] = useState([]);
   const [selectedModel, setSelectedModel] = useState("");
-  const columns = [
-    {
-      title: 'Item',
-      dataIndex: 0,
-      key: 'col-0',
-    },
-    {
-      title: 'Current Value',
-      dataIndex: 1,
-      key: 'col-1',
-    },
-    {
-      title: 'Limit',
-      dataIndex: 2,
-      key: 'col-2',
-    },
-    {
-      title: 'Situation',
-      dataIndex: 3,
-      key: 'col-3'
-    },
-    {
-      title: 'End of life',
-      dataIndex: 4,
-      key: 'col-4',
-    },
-    {
-      title: '',
-      dataIndex: 5,
-      key: 'col-5',
-    },
-    {
-      title: '',
-      dataIndex: 6,
-      key: 'col-6',
-    },
-  ];
-  const columnsResult = [
-    {
-      title: 'No',
-      dataIndex: 'no',
-      key: 'no',
-    },
-    {
-      title: 'Symptom / Detail',
-      dataIndex: 'symptom',
-      key: 'symptom',
-    },
-    {
-      title: 'Remedy',
-      dataIndex: 'remedy',
-      key: 'remedy',
-    },
-    {
-      title: 'Part Code',
-      dataIndex: 'part',
-      key: 'part',
-    },
-  ];
-  const dataResult = [];
-  
-  const propsForDragger = {
-    name: 'file',
-    multiple: false,
-    action: '/api/upload',
-    method: 'post',
-  };
-  const propsCalculate = {
-    name: 'file',
-    multiple: false,
-    action: '/api/analytic/readfile',
-    method: 'post',
-  };
-  // useEffect(() => {
-  //   fetch('/api/manual/listModelSC')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const transformedItems = data.map(item => ({
-  //         key: item.model_name,
-  //         label: item.model_name,
-  //       }));
-  //       setItems(transformedItems);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch('/api/manual/listModelSC')
+      .then(response => response.json())
+      .then(data => {
+        const transformedItems = data.map(item => ({
+          key: item.model_name,
+          label: item.model_name,
+        }));
+        setItems(transformedItems);
+      });
+  }, []);
   
   const handleModelSelect = model => {
     setSelectedModel(model);
@@ -120,7 +201,7 @@ export default function IntrlligentDetail() {
   const [errorDataTable, setTableData] = useState([]);
   const handleUpload = async (file) => {
     try {
-      if (_.isEmpty(selectModel2)) {
+      if (selectedModel === "") {
         // Show an error message using Ant Design message.error
         message.error("Please select a model before uploading.");
         return; // Exit the function
@@ -138,13 +219,13 @@ export default function IntrlligentDetail() {
         // console.log(response.data.data);
         setResponseData(response.data.data);
         setErrorData(response.data.errorData);
-        console.log(selectModel2);
+        console.log(selectedModel);
         console.log(response.data.errorData[0].symptom);
         let symptomResponse = response.data.errorData[0].symptom;
         try {
           
           const responseErr = await axios.post('/api/errorCode/find', {
-            model: selectModel2?.model_name,
+            model: selectedModel,
             errorCode: symptomResponse,
           });
           console.log(responseErr);
@@ -194,17 +275,17 @@ export default function IntrlligentDetail() {
               <Menu>
                 {itemsModel.map(item => (
                   <Menu.Item key={item.key}>
-                    <Button type="link" onClick={() => handleModelSelect(item.key)}>{item.label}</Button>
+                    <a onClick={() => handleModelSelect(item.key)}>{item.label}</a>
                   </Menu.Item>
                 ))}
               </Menu>
             }
           >
-            <Button type="link" onClick={e => e.preventDefault()}>
+            <a onClick={e => e.preventDefault()}>
               <Space>
                 Select <DownOutlined />
               </Space>
-            </Button>
+            </a>
           </Dropdown>
           {selectedModel && (
             <p>Selected Model: {selectedModel}</p>
@@ -214,7 +295,7 @@ export default function IntrlligentDetail() {
       <Row justify="center">
         <Col span={20} style={{ margin: '10px' }}>
           <Dragger
-            {...propsForDragger}
+            {...props}
             onChange={(info) => {
               const { status, originFileObj } = info.file;
               if (status === 'done') {
